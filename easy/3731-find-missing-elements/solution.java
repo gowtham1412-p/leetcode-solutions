95e11bb098b091aa34f7b58b761cfdd87fc05d10
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-18 17:03:34 +0530

    ✅ Find Missing Elements | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-18

--- easy/3731-find-missing-elements/solution.java
+++ easy/3731-find-missing-elements/solution.java
@@ -1,11 +1,12 @@
             }
             sum+=num;
         }
-        long sum1=(long) (maxv)*(maxv+1)/2;
-        long sum2=(long) (minv)*(minv-1)/2;
+                minv=num;
+            }if(num<minv){
+        long sum1=(long) maxv*(maxv+1)/2;
+        long sum2=(long) minv*(minv-1)/2;
 
         long msng=sum1-sum2-sum;
 
         return new ArrayList<>(List.of((int) msng));
     }
-}
