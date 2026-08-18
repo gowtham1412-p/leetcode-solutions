a1fb12ac227a4fb28cd695bd715e92bbb1b674b8
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-18 17:19:36 +0530

    ✅ Find Missing Elements | Easy | Java | Runtime: 5 ms (46.28%) | Memory: 46.7 MB | 2026-08-18

--- easy/3731-find-missing-elements/solution.java
+++ easy/3731-find-missing-elements/solution.java
@@ -1,21 +1,21 @@
-class Solution {
-    public List<Integer> findMissingElements(int[] nums) {
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            max=Math.max(max,nums[i]);
-            min=Math.min(min,nums[i]);
-        }
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            list.add(nums[i]);
-        }
-        List<Integer> array=new ArrayList<>();
-        for(int i=min;i<=max;i++){
-            if(!list.contains(i)){
-        }
-                array.add(i);
-            }
-    }
-        return array;
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,nums[i]);
+            min=Math.min(min,nums[i]);
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            list.add(nums[i]);
+        }
+        List<Integer> array=new ArrayList<>();
+        for(int i=min;i<=max;i++){
+            if(!list.contains(i)){
+                array.add(i);
+            }
+        }
+        return array;
+    }
 }
