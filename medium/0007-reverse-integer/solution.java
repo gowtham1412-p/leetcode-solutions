ff5fef0bc33807c3bfa3d965c150ddbd496f5580
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-18 16:18:49 +0530

    ✅ Reverse Integer | Medium | Java | Runtime: 0 ms | Memory: 42.1 MB | 2026-08-18

--- medium/0007-reverse-integer/solution.java
+++ medium/0007-reverse-integer/solution.java
@@ -1,12 +1,12 @@
-class Solution {
-    public int reverse(int x) {
-        int digit;
-        int rev=0;
-        while(x!=0){
-            digit=x%10;
-            rev=rev*10+digit;
-        }
-    }
-            x=x/10;
-        return rev;
+class Solution {
+    public int reverse(int x) {
+        int digit;
+        int rev=0;
+        while(x!=0){
+            digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+        }
+        return rev;
+    }
 }
