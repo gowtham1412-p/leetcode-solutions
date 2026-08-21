6ffad27069856d04674954833cf3b7b0ac909f0e
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-21 23:15:10 +0530

    ✅ Find the Index of the First Occurrence in a String | Easy | Java | Runtime: 0 ms | Memory: 42.1 MB | 2026-08-21

---
+++ easy/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+       
+            return haystack.indexOf(needle);
+        }
+    }
+        return -1;
+        if(haystack.contains(needle)){
+}
