78d6eb1ef710b8e12eb673f59fdca8c14acd0ece
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-21 23:24:26 +0530

    ✅ Length of Last Word | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.1 MB | 2026-08-21

--- easy/0058-length-of-last-word/solution.java
+++ easy/0058-length-of-last-word/solution.java
@@ -1,15 +1,15 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
-        s=s.trim();
-        int n=s.length();
-        for(int i=n-1;i>=0;i--){
-            if(s.charAt(i)!=' '){
-                count++;
-            }else if(count>0){
-        }
-                break;
-            }
-    }
-        return count;
-        int count=0;
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int count=0;
+        int n=s.length();
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                count++;
+            }else if(count>0){
+                break;
+            }
+        }
+        return count;
+    }
 }
