b6e976e300912df66e9f9ce4fe6fd790408b80bc
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-23 16:36:05 +0530

    ✅ Sum Game | Medium | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-23

--- medium/1927-sum-game/solution.java
+++ medium/1927-sum-game/solution.java
@@ -1,16 +1,16 @@
-                int maxdiff=Math.abs(blankdiff)/2;
+            int sumdiff=ls-rs;
+            int blankdiff=lb-rb;
+            if(blankdiff==0){
+                if(sumdiff==0){
+                    return false;
+                }else{
+                    return true;
+                }
+            }else if(blankdiff%2!=0){
+                return true;
+            }
+            else{
+                int maxdiff=9*Math.abs(blankdiff)/2;
                 if(Math.abs(sumdiff)==maxdiff){
                     return false;
                 }else{
-                    return true;
-                }
-            }
-
-            else{
-            }
-                return true;
-                }
-            }else if(blankdiff%2!=0){
-                    return true;
-                }else{
-                    return false;
