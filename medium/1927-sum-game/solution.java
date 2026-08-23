bab3af894d4413bacf07ae50f8cb70d855bbde72
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-23 16:34:29 +0530

    ✅ Sum Game | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-23

---
+++ medium/1927-sum-game/solution.java
@@ -0,0 +1,16 @@
+                int maxdiff=Math.abs(blankdiff)/2;
+                if(Math.abs(sumdiff)==maxdiff){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+
+            else{
+            }
+                return true;
+                }
+            }else if(blankdiff%2!=0){
+                    return true;
+                }else{
+                    return false;
