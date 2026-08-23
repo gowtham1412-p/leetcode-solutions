b3b89e194744393a12c178c4aede9a4cfd7f5173
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-23 08:18:14 +0530

    ✅ Q1. Check ASCII Palindromic | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-23

--- easy/check-ascii-palindromic/solution.java
+++ easy/check-ascii-palindromic/solution.java
@@ -1,1 +1,23 @@
-01101100011001010110010101110100
+class Solution {
+    public boolean isPalindromic(String s) {
+        int l=0;
+        int r=s.length()-1;
+        while(l<r){
+            int lnum=s.charAt(l);
+            int rnum=s.charAt(r);
+            if(!Character.isLetterOrDigit(lnum)){
+                l++;
+            }
+            else if(!Character.isLetterOrDigit(lnum)){
+            r--;
+            }else{
+                if(Character.toLowerCase(lnum)!=Character.toLowerCase(rnum)){
+                    return false;
+                }
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
