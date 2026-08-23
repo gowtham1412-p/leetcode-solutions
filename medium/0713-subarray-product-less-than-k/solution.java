1d91ba4795d5682daa256742258b81a04e28dc38
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-23 21:06:07 +0530

    ✅ Subarray Product Less Than K | Medium | Java | Runtime: 3 ms (39.19%) | Memory: 48.9 MB | 2026-08-23

--- medium/0713-subarray-product-less-than-k/solution.java
+++ medium/0713-subarray-product-less-than-k/solution.java
@@ -1,5 +1,7 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
+       if(k<=1)
+          return 0;
         int product=1;
         int count=0;
         int i=0;
@@ -7,7 +9,7 @@ class Solution {
        for(int j=0;j<n;j++){
         product*=nums[j];
         while(product>=k){
-            product/=nums[i];
+            product /=nums[i];
             i++;
         }
         count+=j-i+1;
