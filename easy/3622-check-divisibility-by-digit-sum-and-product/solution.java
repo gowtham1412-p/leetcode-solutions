70cd7042314fb5d11eedd50380b0d65e07839ff0
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-23 15:55:09 +0530

    ✅ Check Divisibility by Digit Sum and Product | Easy | Java | Runtime: 0 ms | Memory: 41.8 MB | 2026-08-23

--- easy/3622-check-divisibility-by-digit-sum-and-product/solution.java
+++ easy/3622-check-divisibility-by-digit-sum-and-product/solution.java
@@ -1,14 +1,15 @@
         int pro=1;
-        while(n>0){
-            int digit=n%10;
+        while(n1>0){
+            int digit=n1%10;
         }
             sum+=digit;
             pro*=digit;
-            n/=10;
+            n1/=10;
         
  
         int sum=0;
     public boolean checkDivisibility(int n) {
         return n%(sum+pro)==0;
     }
-}
+class Solution {
+        int n1=n;
