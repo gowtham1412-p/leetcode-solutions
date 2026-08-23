66f130eb6444be6c91d16d635a1ff4dc7b5678b9
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-23 17:11:48 +0530

    ✅ Maximum Sum of Distinct Subarrays With Length K | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-23

--- medium/2461-maximum-sum-of-distinct-subarrays-with-length-k/solution.java
+++ medium/2461-maximum-sum-of-distinct-subarrays-with-length-k/solution.java
@@ -1,16 +1,16 @@
-                set.remove(nums[i]);
-           while(!set.contains(nums[j])){
-            
-           set.add(nums[j]);
-           sum+=nums[j];
-                i++;
            if(j-i+1==k){
-                max=Math.max(max,sum);
+                sum+=j-i;
            }
-                sum-=nums[i];
+                max=Math.max(max,sum);
+                set.remove(nums[i]);
+                i++;
+           j++;
+
+        }
+            set.add(j);
         while(j<n){
         int j=0;
-           j++;
-        }
+        int i=0;
         return max;
-           }
+    }
+}
