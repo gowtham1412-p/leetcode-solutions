50c501d6f7c5ba4c2097cab3e7ab611d79442ee7
gowtham1412-p <gowthampadi692@gmail.com>
2026-08-24 22:49:01 +0530

    ✅ Minimum Size Subarray Sum | Medium | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-08-24

--- medium/0209-minimum-size-subarray-sum/solution.java
+++ medium/0209-minimum-size-subarray-sum/solution.java
@@ -1,15 +1,15 @@
         }
             while(sum>=target){
-              res=Math.max(res,j-i+1);
+              res=Math.min(res,j-i+1);
             }
            
               sum-=nums[i];
               i++;
             sum+=nums[j];
-      return res==Integer.MAX_VALUE ? 0:res;
         for(int j=0;j<n;j++){
         int res=Integer.MAX_VALUE;
         int sum=0;
-        
-}
-}
+        int i=0;
+        int n=nums.length;
+    public int minSubArrayLen(int target, int[] nums) {
+class Solution {
