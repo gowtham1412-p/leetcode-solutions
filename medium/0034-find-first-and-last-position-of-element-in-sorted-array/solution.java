0a656ba836bd482cdc02199189a11d58b043c08a
Gowtham <gowthampadi692@gmail.com>
2026-08-26 23:52:11 +0530

    ✅ Find First and Last Position of Element in Sorted Array | Medium | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-08-26

--- medium/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ medium/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,16 +1,16 @@
+               
+
+            }else if(nums[mid]<target){
+                left=mid+1;
             }else{
                 right=mid-1;
             }
 
         }
+        right=nums.length-1;
         while(left<=right){
             int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 last=mid;
+        left=0;
                 left=mid+1;
-               
-
-            }else if(mid<target){
-                left=mid+1;
-            }else{
-                right=mid-1;
