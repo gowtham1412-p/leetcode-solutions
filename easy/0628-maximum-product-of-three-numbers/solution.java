f6039288ad67f06d943f7a53ebdabb4b7e6b2fec
Gowtham <gowthampadi692@gmail.com>
2026-08-29 07:28:41 +0530

    ✅ Maximum Product of Three Numbers | Easy | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-08-29

---
+++ easy/0628-maximum-product-of-three-numbers/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int pro=1;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            pro*=nums[i];
+        max=Math.max(pro,nums[i]);
+    }
+        }
+        return max;
+        int max=0;
+}
