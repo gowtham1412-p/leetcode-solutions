e0bd9e31982a4f6579ba7d91d71299d97c8fe89d
Gowtham <gowthampadi692@gmail.com>
2026-08-29 08:33:42 +0530

    ✅ Maximum Product of Two Elements in an Array | Easy | Java | Runtime: 5 ms (41.31%) | Memory: 44.6 MB | 2026-08-29

--- easy/1464-maximum-product-of-two-elements-in-an-array/solution.java
+++ easy/1464-maximum-product-of-two-elements-in-an-array/solution.java
@@ -1,14 +1,14 @@
-class Solution {
-    public int maxProduct(int[] nums) {
-        int n=nums.length;
-        int max=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                curr=(nums[i]-1)*(nums[j]-1);
-               max=Math.max(max,curr);
-    }
-        int curr=0;
-        }
-            }
-        return max;
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int max=0;
+        int curr=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                curr=(nums[i]-1)*(nums[j]-1);
+               max=Math.max(max,curr);
+            }
+        }
+        return max;
+    }
 }
