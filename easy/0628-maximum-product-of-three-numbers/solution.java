bb6ad433749f65cdf7d29929d4486acb0ce40343
Gowtham <gowthampadi692@gmail.com>
2026-08-29 08:06:42 +0530

    ✅ Maximum Product of Three Numbers | Easy | Java | Runtime: 15 ms (67.24%) | Memory: 48.1 MB | 2026-08-29

--- easy/0628-maximum-product-of-three-numbers/solution.java
+++ easy/0628-maximum-product-of-three-numbers/solution.java
@@ -1,15 +1,15 @@
-class Solution {
-    public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int left1=nums[0];
-        int left2=nums[1];
-        int right1=nums[n-1];
-        int right2=nums[n-2];
-        int right3=nums[n-3];
-    }
-        int pos=right1*right2*right3;
-        int neg=left1*left2*right1;
-        return Math.max(pos,neg);
-        int n=nums.length;
-        
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        
+        int left1=nums[0];
+        int left2=nums[1];
+        int right1=nums[n-1];
+        int right2=nums[n-2];
+        int right3=nums[n-3];
+        int pos=right1*right2*right3;
+        int neg=left1*left2*right1;
+        return Math.max(pos,neg);
+    }
 }
