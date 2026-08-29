01507b26d43e68669033268c062de24d12e0e3f4
Gowtham <gowthampadi692@gmail.com>
2026-08-29 20:48:46 +0530

    ✅ Separate the Digits in an Array | Easy | Java | Runtime: 5 ms (82.62%) | Memory: 46.5 MB | 2026-08-29

--- easy/2553-separate-the-digits-in-an-array/solution.java
+++ easy/2553-separate-the-digits-in-an-array/solution.java
@@ -1,14 +1,24 @@
-                list.add(temp.get(i));
-              }
-              for(int i=temp.size()-1;i>=0;i--){
-              }
-                num/=10;
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        
+        ArrayList<Integer> list=new ArrayList<>();
 
-        }
-        int arr[]=new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            arr[i]=list.get(i);
-        }
-        return arr;
-    }
+        
+        for(int num:nums){
+        ArrayList<Integer> temp=new ArrayList<>();
+              while(num>0){
+                temp.add(num%10);
+                num/=10;
+              }
+              for(int i=temp.size()-1;i>=0;i--){
+                list.add(temp.get(i));
+              }
+
+        }
+        int arr[]=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
+        }
+        return arr;
+    }
 }
