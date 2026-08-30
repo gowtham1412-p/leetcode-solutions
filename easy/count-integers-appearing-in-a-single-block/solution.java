69954cb440cd8c23946ef7ce2956d374deb67816
Gowtham <gowthampadi692@gmail.com>
2026-08-30 08:16:47 +0530

    ✅ Q1. Count Integers Appearing in a Single Block | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-30

--- easy/count-integers-appearing-in-a-single-block/solution.java
+++ easy/count-integers-appearing-in-a-single-block/solution.java
@@ -1,1 +1,19 @@
-1 <= nums.length <= 100
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            int j=i+1;
+            while(j<n&&nums[j]==nums[i]){
+                j++;
+            }
+            int g=j;
+            while(g<n&&nums[g]!=nums[i])
+                g++;
+            if(g==n)
+               count++;
+            i=j-1;
+        }
+                return count;
+    }
+}
