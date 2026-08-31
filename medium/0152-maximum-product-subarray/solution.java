f871b4653a97281953c5066534c2d1dfcc2915f7
Gowtham <gowthampadi692@gmail.com>
2026-08-31 22:41:45 +0530

    ✅ Maximum Product Subarray | Medium | Java | Runtime: 1 ms (92.91%) | Memory: 47.1 MB | 2026-08-31

--- medium/0152-maximum-product-subarray/solution.java
+++ medium/0152-maximum-product-subarray/solution.java
@@ -1,15 +1,18 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        long n=nums.length;
+        int n=nums.length;
         int max=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-        int pro=1;
-            for(int j=i;j<n;j++){
-              pro*=nums[j];
-            max=Math.max(max,pro);
-            
-            }  
-        }
-        return max;
+        
+        int prefix=1,suffix=1;
+         for(int i=0;i<n;i++){
+            if(prefix==0)
+                 prefix=1;
+            if(suffix==0)
+                suffix=1;
+            prefix*=nums[i];
+            suffix*=nums[n-1-i];
+            max=Math.max(max,Math.max(prefix,suffix));
+         }
+         return max;
     }
 }
