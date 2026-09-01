c733e6a0fa7ae2a0fcde8e01cd628077f45dc633
Gowtham <gowthampadi692@gmail.com>
2026-09-01 20:21:11 +0530

    ✅ Find the Duplicate Number | Medium | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-09-01

--- medium/0287-find-the-duplicate-number/solution.java
+++ medium/0287-find-the-duplicate-number/solution.java
@@ -1,13 +1,12 @@
-class Solution {
-    public int findDuplicate(int[] nums) {
-       int n=nums.length;
        for(int i=0;i<n-1;i++){
         if(nums[i]==nums[i+1]){
        }
             return nums[i];
         
     }
-       int count=0;
+            count++;
+        }
+        if(count>=2){
 }
        return -1;
 }
