56761bc312fd560a21469f9891877499db8c1e1f
Gowtham <gowthampadi692@gmail.com>
2026-09-01 22:15:40 +0530

    ✅ Two Sum II - Input Array Is Sorted | Medium | Java | Runtime: 2 ms (95.63%) | Memory: 48.3 MB | 2026-09-01

--- medium/0167-two-sum-ii-input-array-is-sorted/solution.java
+++ medium/0167-two-sum-ii-input-array-is-sorted/solution.java
@@ -1,12 +1,18 @@
-            }
-        }
-            if(sum>target){
-                right--;
-            }else if(sum<target){
-                left++;
-            }
-               return new int[]{left+1,right+1};
-            if(sum==target){
-        return new int[]{-1,-1};
-    }
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n=numbers.length;
+        int left=0,right=n-1;
+        while(left<right){
+            int sum=numbers[left]+numbers[right];
+            if(sum==target){
+               return new int[]{left+1,right+1};
+            }
+            if(sum>target){
+                right--;
+            }else if(sum<target){
+                left++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
 }
