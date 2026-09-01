902b727d8b90ad827e816d07136e0f736af6e4f6
Gowtham <gowthampadi692@gmail.com>
2026-09-01 10:29:27 +0530

    ✅ Valid Anagram | Easy | Java | Runtime: 3 ms (94.08%) | Memory: 46.5 MB | 2026-09-01

--- easy/0242-valid-anagram/solution.java
+++ easy/0242-valid-anagram/solution.java
@@ -1,15 +1,17 @@
-        Arrays.sort(ch);
-        char chi[]=t.toCharArray();
-        Arrays.sort(chi);
-        for(int i=0;i<ch.length;i++){
-            if(ch[i]!=chi[i]){
-        }
-                return false;
-            }
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        char ch[]=s.toCharArray();
+        Arrays.sort(ch);
+        char chi[]=t.toCharArray();
+        Arrays.sort(chi);
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]!=chi[i]){
+                return false;
 
-        return true;
-        char ch[]=s.toCharArray();
-            return false;
-        if(s.length()!=t.length())
-    public boolean isAnagram(String s, String t) {
-class Solution {
+            }
+        }
+        return true;
+    }
+}
