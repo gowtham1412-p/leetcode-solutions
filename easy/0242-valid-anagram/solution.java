978311749e166f87896b5909fd1e0fedc28ce9e0
Gowtham <gowthampadi692@gmail.com>
2026-09-01 11:20:08 +0530

    ✅ Valid Anagram | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-09-01

--- easy/0242-valid-anagram/solution.java
+++ easy/0242-valid-anagram/solution.java
@@ -1,17 +1,15 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
-            return false;
-        char ch[]=s.toCharArray();
-        Arrays.sort(ch);
-        char chi[]=t.toCharArray();
-        Arrays.sort(chi);
-        for(int i=0;i<ch.length;i++){
-            if(ch[i]!=chi[i]){
-                return false;
-
-            }
-        }
-        return true;
-    }
+            return false;
+        char ch[]=s.toCharArray();
+        Arrays.sort(ch);
+        char chi[]=t.toCharArray();
+        Arrays.sort(chi);
+        
+        if(Arrays.equals(ch,chi)){
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
