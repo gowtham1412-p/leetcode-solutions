996e90a7b664de99d7983a64e78353e2c81c7a65
Gowtham <gowthampadi692@gmail.com>
2026-09-12 20:32:30 +0530

    ✅ Q1. Count Values With Equally Spaced Occurrences I | Easy | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-09-12

--- easy/count-values-with-equally-spaced-occurrences-i/solution.java
+++ easy/count-values-with-equally-spaced-occurrences-i/solution.java
@@ -1,1 +1,34 @@
-3 <= nums.length <= 100
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        int n=nums.length;
+       int count2=0;
+        for(int i=0;i<n;i++){
+            int count1=0;
+            int a1=-1,a2=-1,a3=-1;
+            
+            for(int j=0;j<i;j++){
+                if(nums[j]==nums[i]){
+                    count1++;
+                
+                if(a1==-1){
+                    a1=j;
+                }else if(a2==-1){
+                    a2=j;
+                }else if(a3==-1){
+                    a3=j;
+                }
+                
+            }
+        }
+            if(count1==3 && a2-a1==a3-a2){
+                
+                    count2++;
+                
+            }
+    }
+            
+        
+            return count2;
+        
+    }
+}
